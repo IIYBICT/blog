68aeca7dbe876e51a7586525f9799cd37aa45c21
IIYBICT <630061561@qq.com>
2025-06-29 12:45:26 +0800

    docs: 初始化 VitePress 博客项目
    - 添加基本的项目结构和配置文件
    - 设置 GitHub Actions 部署流程
    - 配置博客主题和样式
    - 添加首页和地图页面
    - 设置机器人访问规则
    - 添加包管理和依赖安装配置

---
+++ docs/.vitepress/blog-theme.ts
@@ -0,0 +1,104 @@
+// 主题独有配置
+import { Theme } from "@sugarat/theme";
+import { getThemeConfig } from "@sugarat/theme/node";
+
+// 开启RSS支持（RSS配置）
+
+const baseUrl = "http://localhost:5173";
+const RSS: Theme.RSSOptions = {
+  title: "IIYBICT Blog",
+  baseUrl,
+  copyright: "Copyright (c) 2025 IIYBICT",
+  description: "",
+  language: "zh-cn",
+  image: "http://q1.qlogo.cn/g?b=qq&nk=630061561&s=100",
+  favicon: "http://q1.qlogo.cn/g?b=qq&nk=630061561&s=100",
+};
+
+// 所有配置项，详见文档: https://theme.sugarat.top/
+const blogTheme = getThemeConfig({
+  // 开启RSS支持
+  RSS,
+
+  // 搜索
+  // 默认开启pagefind离线的全文搜索支持（如使用其它的可以设置为false）
+  // search: false,
+
+  // markdown 图表支持（会增加一定的构建耗时）
+  mermaid: true,
+
+  // 页脚
+  footer: {
+    // message 字段支持配置为HTML内容，配置多条可以配置为数组
+    // message: '下面 的内容和图标都是可以修改的噢（当然本条内容也是可以隐藏的）',
+    copyright: "MIT License | IIYBICT",
+    // icpRecord: {
+    //   name: '蜀ICP备19011724号',
+    //   link: 'https://beian.miit.gov.cn/'
+    // },
+    // securityRecord: {
+    //   name: '公网安备xxxxx',
+    //   link: 'https://www.beian.gov.cn/portal/index.do'
+    // },
+  },
+
+  // 主题色修改
+  themeColor: "el-blue",
+
+  // 文章默认作者
+  author: "IIYBICT",
+
+  // 友链
+  friend: [
+    // {
+    //   nickname: "粥里有勺糖",
+    //   des: "你的指尖用于改变世界的力量",
+    //   avatar:
+    //     "https://img.cdn.sugarat.top/mdImg/MTY3NDk5NTE2NzAzMA==674995167030",
+    //   url: "https://sugarat.top",
+    // },
+    // {
+    //   nickname: "Vitepress",
+    //   des: "Vite & Vue Powered Static Site Generator",
+    //   avatar: "https://vitepress.dev/vitepress-logo-large.webp",
+    //   url: "https://vitepress.dev/",
+    // },
+  ],
+
+  // 公告
+  //   popover: {
+  //     title: "公告",
+  //     body: [
+  //       { type: "text", content: "👇公众号👇---👇 微信 👇" },
+  //       {
+  //         type: "image",
+  //         src: "https://img.cdn.sugarat.top/mdImg/MTYxNTAxODc2NTIxMA==615018765210~fmt.webp",
+  //       },
+  //       {
+  //         type: "text",
+  //         content: "欢迎大家加群&私信交流",
+  //       },
+  //       {
+  //         type: "text",
+  //         content: "文章首/文尾有群二维码",
+  //         style: "padding-top:0",
+  //       },
+  //       {
+  //         type: "button",
+  //         content: "作者博客",
+  //         link: "https://sugarat.top",
+  //       },
+  //       {
+  //         type: "button",
+  //         content: "加群交流",
+  //         props: {
+  //           type: "success",
+  //         },
+  //         link: "https://theme.sugarat.top/group.html",
+  //       },
+  //     ],
+  //     duration: 0,
+  //   },
+});
+
+export { blogTheme };
---
+++ docs/map/index.md
@@ -0,0 +1,8 @@
+---
+sticky: 1
+# sidebar: false
+tag:
+ - Flight Map
+---
+
+# 地图安装教程
